4b769f0fdb5220a4bf56c54bc83fa9b187a50ba5
Animesh Samaiya <asamaiya00@gmail.com>
2022-05-18 23:57:18 +0530

    fetch and show suggestions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [regions, setRegions] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const loadregions = async () => {
+      const res = await axios.get(
+        `https://base.amberstudent.com/api/v0/regions?sort_key=search_name&sort_order=desc&states=active&search_name=${query}`
+      );
+      setRegions(res.data.data.result.splice(0, 5));
+    };
+    loadregions();
+  }, [query]);
+
+  const handleQueryChange = (e) => {
+    console.log(e.target.value);
+    if (e.target.value.length > 0 && e.target.value.length < 3) {
+      setSuggestions([{ name: 'Please enter at least 3 characters' }]);
+    } else if (regions.length === 0) {
+      setSuggestions([{ name: 'No relevant data found' }]);
+    } else {
+      setSuggestions(regions);
+    }
+    console.log(regions);
+    setQuery(e.target.value);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          value={query}
+          onChange={handleQueryChange}
+          onFocus={() => setSuggestions(regions)}
+          onBlur={() => setSuggestions([])}
+        />
+        {suggestions &&
+          suggestions.map((suggestion) => <h3>{suggestion.name}</h3>)}
       </header>
     </div>
   );
